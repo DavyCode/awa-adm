8de5d92310477abcdabf3d888824047b60924639
iAfolayan <iafolayanibikunle@gmail.com>
2023-12-28 13:38:55 +0100

    Implementing dashboard UI

---
+++ src/assets/icons/ArrowDownSolid.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ArrowDownSolid = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        opacity="0.4"
+        d="M10.32 8.81979L7.79333 5.45312H4.05333C3.41333 5.45312 3.09333 6.22646 3.54667 6.67979L7 10.1331C7.55333 10.6865 8.45333 10.6865 9.00667 10.1331L10.32 8.81979Z"
+        fill="white"
+      />
+      <path
+        d="M10.374 8.05872L8.79373 5.95312H11.9467C12.1398 5.95312 12.2406 6.1885 12.1048 6.32791L10.374 8.05872Z"
+        fill="white"
+        stroke="white"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDownSolid;
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,70 @@
+"use client";
 import React from "react";
+import Dropdown from "../Dropdowns/selectOption";
+import { SortByOptions, FilterByOptions } from "@/lib/data";
+import { ArrowDown } from "lucide-react";
+import CalendarIcon from "@/assets/icons/CalendarIcon";
+import ArrowDownSolid from "@/assets/icons/ArrowDownSolid";
+
+interface PastDateProps {
+  label: string;
+  value: string;
+  icon?: JSX.Element;
+  arrowIcon?: JSX.Element;
+}
+
+const FilterByPastDate: PastDateProps[] = [
+  {
+    label: "Past 30 days",
+    value: "Past 30 days",
+  },
+  {
+    label: "Past 7 days",
+    value: "Past 7 days",
+  },
+  {
+    label: "Custom date",
+    value: "Custom date",
+    icon: <CalendarIcon />,
+    arrowIcon: <ArrowDownSolid />,
+  },
+];
 
 const DashboardComponent = () => {
+  const handleSelect = (selectedOption: string) => {
+    console.log(`Selected: ${selectedOption}`);
+    // Perform any other actions based on the selected option
+  };
+
   return (
     <div className="bg-primary w-full sm:max-w-[1440px] sm:h-[259px] pt-[46px] pr-20 pb-[151px] pl-[85px] flex border-t border-[#F1f1f1]/30">
-      DashboardComponent
+      <div className="flex justify-between items-center w-full">
+        <div className="flex flex-col">
+          <span className="text-sm font-normal text-white/80">
+            Dashboard Overview
+          </span>
+          <p className="text-2xl font-medium sm:max-w-[225px] text-white mt-1">
+            Welcome Back Tunji
+          </p>
+        </div>
+        <div className="w-full sm:w-[393px] flex justify-between items-center gap-2">
+          <Dropdown
+            options={SortByOptions}
+            name="Sort by"
+            onSelect={handleSelect}
+          />
+          <Dropdown
+            options={FilterByOptions}
+            name="Filter by"
+            onSelect={handleSelect}
+          />
+          <Dropdown
+            options={FilterByPastDate}
+            name="Past 30 days"
+            onSelect={handleSelect}
+          />
+        </div>
+      </div>
     </div>
   );
 };
